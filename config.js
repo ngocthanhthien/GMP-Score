067cf4785c04f2a3d27ebf01d12e5e8b80a9f930
Đặng Thanh Bình <dangthanhbinh53@gmail.com>
2026-09-16 23:01:27 +0700

    change

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 // TUYỆT ĐỐI KHÔNG điền vào đây: sb_secret_..., service_role key, hay mật khẩu
 // database. File này được publish công khai trên GitHub Pages cùng index.html.
 window.GMP_SUPABASE_CONFIG = {
-  url: "https://YOUR_PROJECT_REF.supabase.co",
-  anonKey: "sb_publishable_xxx_hoac_anon_key_cua_ban",
+  url: "https://thhevdrgbxvyatfvtyrh.supabase.co",
+  anonKey: "sb_publishable_Hwaw_tr5gn4qeTeP9mz-Hw_unvKOr2Q",
 };
